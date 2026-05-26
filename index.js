8e7be0f5d9352fc4ca8634f5de83b60b8f02bee6
micminem <micminem@gmail.com>
2026-05-26 10:51:36 +0100

    my-first-api

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: "Hello from my PC API!" });
+});
+
+app.get('/joke', (req, res) => {
+  res.json({ joke: "Why did the dev quit? He had no branches to hang onto" });
+});
+app.get('/kist', (req, res) => {
+    res.json({ kist: "Kist is a term used in some regions to refer to a chest or box, often used for storage." });
+}
+);
+app.get('/micheal', (req, res) => {
+    res.json({ micheal: "Michael is a name that means 'who is like God'." });
+});
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
